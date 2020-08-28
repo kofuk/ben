d6360def625f67a705e78f9b255ba15b575d4c1b
Koki Fukuda <ko.fu.dev@gmail.com>
2020-08-29 00:16:24 +0900

    Use getopt option parser

--- src/main.cc
+++ src/main.cc
@@ -18,18 +18,69 @@
 
 #include <iostream>
 
+#include <getopt.h>
+
 #include "command.hh"
-#include "interactive.hh"
 #include "file.hh"
+#include "interactive.hh"
+
+/* Simply, to make settings for code completion easy. */
+#ifndef VERSION_MAJOR
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#endif
+
+namespace {
+    void print_usage() {
+        std::cout << R"(usage: ben [OPTION]... [FILE]...
+Load FILE's to buffer for analysis then launch interactive
+command line.
+
+  -h, --help     Print this message and exit.
+  -v, --version  Print version and exit.
+)";
+    }
+
+    void print_version() {
+        std::cout << "ben "
+                  << VERSION_MAJOR << '.' << VERSION_MINOR << '.'
+                  << VERSION_PATCH << '\n';
+    }
+
+    struct option options[] = {
+        {"help", no_argument, nullptr, 'h'},
+        {"version", no_argument, nullptr, 'v'},
+        {0, 0, 0, 0},
+    };
+
+} // namespace
 
 int main(int argc, char **argv) {
+    for (;;) {
+        int c = getopt_long(argc, argv, "hv", options, nullptr);
+        if (c == -1) break;
+
+        switch (c) {
+        case 'h':
+            print_usage();
+            return 0;
+        case 'v':
+            print_version();
+            return 0;
+        default:
+            return 1;
+        }
+    }
+
     ben::uni_init();
     ben::file_init();
     ben::printer_init();
 
     std::cout << "Loading files...\n";
-    for (int i = 1; i < argc; ++i) {
-         ben::load_file(argv[i]);
+    for (int i = optind; i < argc; ++i) {
+        std::cout << " - Loading " << argv[i] << "...\n";
+        ben::load_file(argv[i]);
     }
     ben::list_file();
 
