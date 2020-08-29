946895001270f8d29a10a1861183cb8b404e0c90
Koki Fukuda <ko.fu.dev@gmail.com>
2020-08-29 09:43:21 +0900

    Add string printer

--- src/printer.cc
+++ src/printer.cc
@@ -18,8 +18,11 @@
 
 #include <algorithm>
 #include <bitset>
+#include <cctype>
 #include <cstdint>
 #include <cstring>
+#include <exception>
+#include <iomanip>
 #include <ios>
 #include <iostream>
 #include <string>
@@ -29,28 +32,6 @@
 
 namespace ben {
     namespace {
-        void help_print([[maybe_unused]] std::string cmd) {
-            std::cout << R"(print TYPE [bin|dec|hex] [BUFFER]
-Interpret byte array beginning from cursor position as given TYPE
-and print.
-You can check and specify byte-order with `endian' command.
-If BUFFER is omitted, use previously used buffer.
-
-Possible types;
-  char    ASCII character.
-  uint8   unsigned int with 8-bit width.
-  uint16  unsigned int with 16-bit width.
-  uint32  unsigned int with 32=bit width.
-  uint64  unsigned int with 64-bit width.
-  int8    two's complement 8-bit integer.
-  int16   two's complement 16-bit integer.
-  int32   two's complement 32-bit integer.
-  int64   two's complement 64-bit integer.
-  float   IEEE 754 single precision floating point number.
-  double  IEEE 754 double precision floating point number.
-)";
-        }
-
         bool big_endian;
 
         void help_endian([[maybe_unused]] std::string cmd) {
@@ -77,6 +58,40 @@ if big or little is specified, prints current value.
             return 0;
         }
 
+        void print_char(char const c) {
+            if (std::isprint(c)) {
+                std::cout << c;
+            } else {
+                std::ios init(nullptr);
+                init.copyfmt(std::cout);
+                std::cout << "\\x" << std::hex << std::setw(2)
+                          << std::setfill('0') << +c;
+                std::cout.copyfmt(init);
+            }
+        }
+
+        void help_print([[maybe_unused]] std::string cmd) {
+            std::cout << R"(print TYPE [bin|oct|dec|hex] [BUFFER]
+Interpret byte array beginning from cursor position as given TYPE
+and print.
+You can check and specify byte-order with `endian' command.
+If BUFFER is omitted, use previously used buffer.
+
+Possible types;
+  char    ASCII character.
+  uint8   unsigned int with 8-bit width.
+  uint16  unsigned int with 16-bit width.
+  uint32  unsigned int with 32=bit width.
+  uint64  unsigned int with 64-bit width.
+  int8    two's complement 8-bit integer.
+  int16   two's complement 16-bit integer.
+  int32   two's complement 32-bit integer.
+  int64   two's complement 64-bit integer.
+  float   IEEE 754 single precision floating point number.
+  double  IEEE 754 double precision floating point number.
+)";
+        }
+
         inline void ordered_memcpy(void *dest, void *src, size_t n) {
             if (big_endian) {
                 std::uint8_t buf[n];
@@ -92,7 +107,7 @@ if big or little is specified, prints current value.
             return f->data.size() - f->cursor >= size;
         }
 
-        enum class print_style { BIN, DEC, HEX };
+        enum class print_style { BIN, OCT, DEC, HEX };
 
         template <typename T> void print_value(T value, print_style sty) {
             if (sty == print_style::BIN) {
@@ -101,7 +116,9 @@ if big or little is specified, prints current value.
                 std::ios init(nullptr);
                 init.copyfmt(std::cout);
 
-                if (sty == print_style::DEC)
+                if (sty == print_style::OCT) {
+                    std::cout << std::oct;
+                } else if (sty == print_style::DEC)
                     std::cout << std::dec;
                 else if (sty == print_style::HEX)
                     std::cout << std::hex;
@@ -125,6 +142,8 @@ if big or little is specified, prints current value.
             if (args.size() >= 3) {
                 if (args[2] == "bin") {
                     style = print_style::BIN;
+                } else if (args[2] == "oct") {
+                    style = print_style::OCT;
                 } else if (args[2] == "dec") {
                     style = print_style::DEC;
                 } else if (args[2] == "hex") {
@@ -153,7 +172,8 @@ if big or little is specified, prints current value.
             std::string type = args[1];
             if (type == "char"s) {
                 if (!check_buffer_size(f, 1)) return 1;
-                std::cout << (char)f->data[f->cursor] << '\n';
+                print_char(f->data[f->cursor]);
+                std::cout << '\n';
             } else if (type == "uint8"s) {
                 if (!check_buffer_size(f, 1)) return 1;
                 print_value(f->data[f->cursor], style);
@@ -207,10 +227,69 @@ if big or little is specified, prints current value.
 
             return 0;
         }
+
+        void help_str([[maybe_unused]] std::string cmd) {
+            std::cout << R"(usage: string [LEN [BUF]]
+Print LEN bytes beginning from cursor as ASCII string.
+If LEN is not specified, prints following printable ASCII
+characters.
+)";
+        }
+
+        int str(std::vector<std::string> const &args) {
+            bool len_specified = false;
+            std::size_t len = 0;
+            file *f;
+            if (args.size() >= 2) {
+                len_specified = true;
+                try {
+                    len = std::stoul(args[1]);
+                } catch (std::exception const &) {
+                    std::cout << "Invalid length.\n";
+                    return 1;
+                }
+                if (args.size() >= 3) {
+                    f = get_file(args[2]);
+                } else {
+                    f = get_file("");
+                }
+            } else {
+                f = get_file("");
+            }
+
+            if (!f) {
+                std::cout << "Invalid buffer.\n";
+                return 1;
+            }
+
+            if (len_specified) {
+                for (std::size_t point = f->cursor;
+                     point < f->data.size() && point < f->cursor + len; ++point)
+                    print_char(f->data[point]);
+
+                std::cout << '\n';
+            } else {
+                bool any = false;
+                for (std::size_t point = f->cursor; point < f->data.size();
+                     ++point) {
+                    if (std::isprint(f->data[point])) {
+                        print_char(f->data[point]);
+                        any = true;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (any) std::cout << '\n';
+            }
+
+            return 0;
+        }
     } // namespace
 
     void printer_init() {
         command_register("print", &print, &help_print);
         command_register("endian", &endian, &help_endian);
+        command_register("string", &str, &help_str);
     }
 } // namespace ben
